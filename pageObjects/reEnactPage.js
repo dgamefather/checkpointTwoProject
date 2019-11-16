44a5f7c412eaf7c1d7b1f31bcaf884435c51b012
Brennan Litster <dgamefather@thatoneplace.net>
2019-11-16 15:07:37 -0700

    Got tests working, wanting to make object and name filter tests with for loops

--- pageObjects/reEnactPage.js
+++ pageObjects/reEnactPage.js
@@ -1,6 +1,28 @@
-module.export = {
+module.exports = {
     url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
     elements: {
-        
+        // Inputs
+        evenOdd: '[name="evenOddInput"]',
+        objFilter: '[name="objectFilterInput"]',
+        nameFilter: '#nameFilterInput',
+        paladinInput: '[name="palindromeInput"]',
+        oneSum: '[name="sumInput1"]',
+        twoSum: '[name="sumInput2"]',
+
+        //Buttons
+        evenOddBtn: '[name="evenOddButton"]',
+        objBtn: '[name="objectFilterButton"]',
+        nameBtn: '#nameFilterButton',
+        paladinBtn: '[name="palindromeButton"]',
+        sumBtn: '[name="sumButton"]',
+
+        // Verify Fields
+        page: '.sumPB',
+        vEven: '[name="evenResults"]',
+        vOdd: '[name="oddResults"]',
+        vObj: '[name="objectFilterResults"]',
+        vName: '[name="nameFilterResults"]',
+        vPaladin: '[name="palindromeResults"]',
+        vSum: '[name="sumResults"]',
     }
 }
--- tests/reEnact.js
+++ tests/reEnact.js
@@ -1,5 +1,5 @@
 var enact = {}
-module.export = {
+module.exports = {
     beforeEach: browser => {
         enact = browser.page.reEnactPage();
         enact
@@ -9,7 +9,77 @@ module.export = {
         browser
             .end();
     },
-    'Test Here': browser => {
+    'Even Number': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@evenOdd', '64')
+            .click('@evenOddBtn')
+            .verify.containsText('@vEven', '64');
+    },
+    'Odd Number': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@evenOdd', '255')
+            .click('@evenOddBtn')
+            .verify.containsText('@vOdd', '255');
+    },
+    'Object Filter': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@objFilter', 'hairColor')
+            .click('@objBtn')
+            .verify.containsText('@vObj', 'hairColor');
+    },
+    'Name Filter': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@nameFilter', 'Jennifer')
+            .click('@nameBtn')
+            .verify.containsText('@vName', 'Jennifer');
+    },
+    'Palindrome - True': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@paladinInput', 'a = a')
+            .click('@paladinBtn')
+            .verify.containsText('@vPaladin', 'true');
+    },
+    'Palindrome - False': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@paladinInput', 'a = b')
+            .click('@paladinBtn')
+            .verify.containsText('@vPaladin', 'false');
+    },
+    'Addition': browser => {
+        enact
+            .verify.visible('@page')
+            .setValue('@oneSum', '123')
+            .setValue('@twoSum', '132')
+            .click('@sumBtn')
+            .verify.containsText('@vSum', '255');
+    },
 
-    }
+    // 'Object Filter': browser => {
+    //     var objection = ["name", "hairColor", "age", "title"];
+    //     for (var x = 0; x < objection.length; x++) {
+    //         enact
+    //             .verify.visible('@page')
+    //             .setValue({ selector: '@objFilter', input: objection[x] })
+    //             .click('@objBtn')
+    //             .verify.containsText('@vObj', objection[x])
+    //             .clearValue('@objFilter')
+    //     }
+    // },
+    // 'Name Filter': browser => {
+    //     var name = ["James", "Jessica", "Melody", "Tyler", "Blake", "Jennifer", "Mark", "Maddy"];
+    //     for (var x = 0; x < name.length; x++) {
+    //         enact
+    //             .verify.visible('@page')
+    //             .setValue({ selector: '@nameFilter', input: name[x] })
+    //             .click('@nameBtn')
+    //             .verify.containsText('@vName', name[x])
+    //             .clearValue('@nameFilter')
+    //     }
+    // },
 }
